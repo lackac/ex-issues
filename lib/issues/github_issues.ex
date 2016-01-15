be9922493dbe336e913f7f118cc283fa363db716
Laszlo Bacsi <lackac@lackac.hu>
2016-01-15 17:02:27 +0100

    Put GitHub API URL into configuration

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,6 +1,8 @@
 defmodule Issues.GitHubIssues do
   @user_agent [{"User-Agent", "Elixir elixir@lackac.hu"}]
 
+  @github_url Application.get_env(:issues, :github_url)
+
   def fetch({user, repo}) do
     issues_url(user, repo)
       |> HTTPoison.get(@user_agent)
@@ -8,7 +10,7 @@ defmodule Issues.GitHubIssues do
   end
 
   def issues_url(user, repo) do
-    "https://api.github.com/repos/#{user}/#{repo}/issues"
+    "#{@github_url}/repos/#{user}/#{repo}/issues"
   end
 
   def handle_response({:ok,    %{status_code: 200, body: body}}) do
