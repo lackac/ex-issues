a2e18011de64c81d73c84c31d4ed7e4774b439ff
Laszlo Bacsi <lackac@lackac.hu>
2016-01-15 16:40:58 +0100

    Fetch GitHub issues for the specified repo using HTTPoison

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,16 @@
+defmodule Issues.GitHubIssues do
+  @user_agent [{"User-Agent", "Elixir elixir@lackac.hu"}]
+
+  def fetch({user, repo}) do
+    issues_url(user, repo)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
+  end
+
+  def issues_url(user, repo) do
+    "https://api.github.com/repos/#{user}/#{repo}/issues"
+  end
+
+  def handle_response({:ok,    %{status_code: 200, body: body}}), do: {:ok, body}
+  def handle_response({:error, %{status_code: ___, body: body}}), do: {:error, body}
+end
